f1a2f51d6dbe50e03070f0cdd4d66284ddfa74e8
Documenter.jl <documenter@juliadocs.github.io>
2024-11-04 07:04:58 +0000

    build based on 5d267e8

---
+++ dev/search_index.js
@@ -0,0 +1,3 @@
+var documenterSearchIndex = {"docs":
+[{"location":"","page":"Home","title":"Home","text":"CurrentModule = TinyTEBD","category":"page"},{"location":"#TinyTEBD","page":"Home","title":"TinyTEBD","text":"","category":"section"},{"location":"","page":"Home","title":"Home","text":"Documentation for TinyTEBD.","category":"page"},{"location":"","page":"Home","title":"Home","text":"","category":"page"},{"location":"","page":"Home","title":"Home","text":"Modules = [TinyTEBD]","category":"page"}]
+}
